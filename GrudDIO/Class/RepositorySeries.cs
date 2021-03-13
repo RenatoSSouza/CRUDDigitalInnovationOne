49803f217bee536ffadc8f99f8b5a9ad7d06cb95
Renato Souza <renato.ssouza@live.com>
2021-03-12 22:41:00 -0300

    Criado classe RepositorySeries, a interface IRepository e suas interfaces ainda não implementadas

---
+++ GrudDIO/Class/RepositorySeries.cs
@@ -0,0 +1,41 @@
+﻿using GrudDIO.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrudDIO.Class
+{
+    class RepositorySeries : IRepository<Series>
+    {
+        private List<Series> SeriesList = new List<Series>();
+        public void Exclude(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Series entityes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Series> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int NextId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Series ReturnById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Series entityes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GrudDIO/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrudDIO.Interfaces
+{
+    public interface IRepository
+    {
+        List<T> list();
+        T ReturnById(int id);
+        void Insert(T entityes);
+        void Exclude(int id);
+        void Update(int id, T entityes);
+        int NextId();
+    }
+}
